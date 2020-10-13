701ede743d5e198b7d908077bd3620438f8399f6
Whiddon Sibdhannie <59574714+Whiddon3@users.noreply.github.com>
2020-10-13 16:26:57 -0400

    Pull spotify api data

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template, redirect
+from data import spotify
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello():
-    return "Hello World!"
+    return "This is our main page"
+
+@app.route('/wip')
+def get_user_data():
+    # @todo: add authorization to GET request
+    # data = spotify.get_recent_tracks(url, auth_header)
+    return "This a work in progress!"
 
 if __name__ == '__main__':
-    app.run()
+    app.run(use_reloader=True, debug=True)
---
+++ spotify/spotify.py
@@ -0,0 +1,13 @@
+import requests
+
+RECENT_TRACKS_ENDPOINT = 'https://api.spotify.com/v1/me/player/recently-played'
+
+# To do:
+# - User Authroization
+
+# https://developer.spotify.com/web-api/web-api-personalization-endpoints/get-recently-played/
+def get_recent_tracks(auth_header):
+    url = RECENT_TRACKS_ENDPOINT
+    # @todo: add authorization to GET request
+    # resp = requests.get(url, auth_header)
+    # return resp.json()
