2322d9faa5f81b3ce53c5ac739d34bd51b5f0720
Whiddon Sibdhannie <59574714+Whiddon3@users.noreply.github.com>
2020-10-30 17:58:21 -0400

    Update spotify api endpoint
    By default, spotify will return 10 songs. We can extend this to 50.

--- spotify_data/spotify.py
+++ spotify_data/spotify.py
@@ -1,4 +1,5 @@
-RECENT_TRACKS_ENDPOINT = 'https://api.spotify.com/v1/me/player/recently-played'
+# Get the Current User's Recently Played Tracks (50)
+RECENT_TRACKS_ENDPOINT = 'https://api.spotify.com/v1/me/player/recently-played?limit=50'
 
 # To do:
 # - User Authroization
@@ -6,6 +7,6 @@ RECENT_TRACKS_ENDPOINT = 'https://api.spotify.com/v1/me/player/recently-played'
 # https://developer.spotify.com/web-api/web-api-personalization-endpoints/get-recently-played/
 def get_recent_tracks(auth_header):
     url = RECENT_TRACKS_ENDPOINT
-    # @todo: add authorization to GET request
-    # resp = requests.get(url, auth_header)
+    # @todo: use authorization from frontend
+    # resp = requests.get(url, headers=auth_header)
     # return resp.json()
