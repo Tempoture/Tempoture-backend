662716916015414d2bb8c9cafe1b471e727c925a
Jimmers2001 <jdbatista2001@gmail.com>
2020-11-22 13:59:29 -0500

    Update app.py
    starting connection between front end and back end while also beginning fixes on database connection

--- app.py
+++ app.py
@@ -2,15 +2,6 @@ import requests
 from flask import Flask, request, jsonify, redirect
 from spotify_data import spotify
 from weather_data import weather
-import psycopg2
-import urllib.parse as urlparse
-import os
-
-url = urlparse.urlparse(os.environ['DATABASE_URL'])
-db = "dbname=%s user=%s password=%s host=%s " % (url.path[1:], url.username, url.password, url.hostname)
-
-connection = psycopg2.connect(db)
-cur = connection.cursor()
 
 app = Flask(__name__)
 
@@ -19,6 +10,14 @@ def weather_json():
     return data
 
 @app.route('/', methods=['GET'])
+def api():
+    return {
+        'userID': 1,
+        'title': 'Flask and React Application.',
+        'completed': False
+    }
+
+@app.route('/songs', methods=['GET'])
 def spotify_user_songs():
     access_token = request.args.get('access_token')
     data = spotify.get_recent_tracks(access_token)
