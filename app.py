1aac30b92dafbf9c48862c4bee5ee4444685b082
Whiddon Sibdhannie <59574714+Whiddon3@users.noreply.github.com>
2020-11-24 15:47:30 -0500

    Parse weather data from API

--- app.py
+++ app.py
@@ -5,10 +5,6 @@ from weather_data import weather
 
 app = Flask(__name__)
 
-def weather_json():
-    data = weather.get_current_weather()
-    return data
-
 @app.route('/', methods=['GET'])
 def api():
     return {
@@ -22,6 +18,34 @@ def spotify_user_songs():
     access_token = request.args.get('access_token')
     data = spotify.get_recent_tracks(access_token)
     return data
+
+@app.route('/weather')
+def get_weather():
+    zipcode = weather.get_zip_code()
+    data = weather.get_current_weather()
+    avg_temp = weather.get_avg_temp(data)
+    pressure = weather.get_pressure(data)
+    humidity = weather.get_humidity(data)
+    weather_id = weather.get_weather_id(data)
+    wind_speed = weather.get_wind_speed(data)
+    wind_direction = weather.get_wind_direction(data)
+    cloudiness = weather.get_cloudiness(data)
+    precipitation = weather.get_precipitation(data)
+    last_updated = weather.get_last_updated(data)
+
+    # print(zipcode)
+    # print(avg_temp)
+    # print(pressure)
+    # print(humidity)
+    # print(weather_id)
+    # print(wind_speed)
+    # print(wind_direction)
+    # print(cloudiness)
+    # print(precipitation)
+    # print(last_updated)
+
+    return data
+
 
 if __name__ == '__main__':
     app.run(use_reloader=True, debug=True)
--- weather_data/weather.py
+++ weather_data/weather.py
@@ -1,24 +1,118 @@
 import requests
+import xmltodict
 from flask import Flask, request, jsonify, redirect
 
-def get_current_weather_json():
+# API KEYS
+GEO_KEY = os.environ['GEO_API_KEY']
+WEATHER_KEY = os.environ['WEATHER_API_KEY']
+
+# Return user's zip code
+def get_zip_code():
     CLIENT_IP = request.remote_addr
-    GEO_KEY = os.environ['GEO_API_KEY']
-    WEATHER_KEY = os.environ['WEATHER_API_KEY']
+    CURRENT_GEO_ENDPOINT = 'https://api.ipgeolocation.io/ipgeo?apiKey={0}&ip={1}&fields=geo'.format(GEO_KEY,CLIENT_IP)    
+    r = requests.get(CURRENT_GEO_ENDPOINT)
+    thedict=r.json()
+    try:
+        zipcode=thedict['zipcode']
+    except Exception:
+        return '00000'
+    return zipcode
+
+# Note from OpenWeather API:
+# If you do not see some of the parameters in your API response it means that these weather 
+# phenomena are just not happened for the time of measurement for the city or location chosen. 
+# Only really measured or calculated data is displayed in API response.
+def get_current_weather():
+    CLIENT_IP = request.remote_addr
     CURRENT_GEO_ENDPOINT = 'https://api.ipgeolocation.io/ipgeo?apiKey={0}&ip={1}&fields=geo'.format(GEO_KEY,CLIENT_IP)    
     r = requests.get(CURRENT_GEO_ENDPOINT)
     thedict=r.json()
     try:
         zipcode=thedict['zipcode']
         countrycode=thedict['country_code2']
-        wr = requests.get('http://api.openweathermap.org/data/2.5/weather?zip={0},{1}&appid={2}'.format(zipcode,countrycode,WEATHER_KEY))
-        weatherdict=wr.json()
+        units_type = 'imperial'
+        wr = requests.get('http://api.openweathermap.org/data/2.5/weather?zip={0},{1}&mode=xml&appid={2}&units={3}'.format(zipcode,countrycode,WEATHER_KEY,units_type))
+        weatherdict = xmltodict.parse(wr.content)
     except Exception:
         return dict()
     return weatherdict
 
-def get_current_weather():
-    if bool(get_current_weather_json()):
-        if get_current_weather_json()['cod'] == 200:
-            return get_current_weather_json()['weather'][0]
-    return dict()
+
+# Weather Data for User
+# Table includes:
+#   - daily_forecast_id
+#   - date
+#   - average_temp
+#   - pressure
+#   - humidity
+#   - weather_id
+#   - wind_speed
+#   - wind_direction
+#   - cloudiness
+#   - precipitation_volume
+#   - snow_volume ?
+#   - precipitation_probability ?
+#   - updated_at
+
+# in fahrenheit
+def get_avg_temp(data):
+    try:
+        return data['current']['temperature']['@value']
+    except Exception:
+        return -1
+
+# in hPa
+def get_pressure(data):
+    try:
+        return data['current']['pressure']['@value']
+    except Exception:
+        return -1
+
+# in %
+def get_humidity(data):
+    try:
+        return data['current']['humidity']['@value']
+    except Exception:
+        return -1
+
+# weather code
+def get_weather_id(data):
+    try:
+        return data['current']['weather']['@number']
+    except Exception:
+        return -1
+
+# in mph
+def get_wind_speed(data):
+    try:
+        return data['current']['wind']['speed']['@value']
+    except Exception:
+        return -1
+
+# in degrees
+def get_wind_direction(data):
+    try:
+        return data['current']['wind']['direction']['@value']
+    except Exception:
+        return -1
+
+# in %  , 0 - 1
+def get_cloudiness(data):
+    try:
+        return data['current']['clouds']['@value']
+    except Exception:
+        return -1
+
+# in mm
+def get_precipitation(data):
+    try:
+        return data['current']['precipitation']['@value']
+    except Exception:
+        return 0
+
+# ex. "2020-11-24T20:36:00"
+def get_last_updated(data):
+    try:
+        return data['current']['lastupdate']['@value']
+    except Exception:
+        return -1
