b920d125bdede8d4f6d324e81e9fc294b008c780
Whiddon Sibdhannie <whiddon3@gmail.com>
2021-03-29 13:44:53 -0400

    Add full function to create custom playlists

--- tests/test_app.py
+++ tests/test_app.py
@@ -5,7 +5,8 @@ from spotifyclient import SpotifyClient
 def main():
     SpotifyObj = SpotifyClient(os.getenv("SPOTIFY_ACCESS_TOKEN"),
                                    os.getenv("SPOTIFY_USER_ID"))
-    print(SpotifyObj.test_create_and_populate("Tempoture", "Cool New Playlist"))
+    SpotifyObj.create_tempoture_playlist(SpotifyObj.get_recent_tracks())
+    #print(SpotifyObj.test_create_and_populate("Tempoture", "Cool New Playlist"))
     #print(SpotifyObj.create_playlist("Tempoture", "Tempoture's Cool Custom Playlist For You!"))
 
 
