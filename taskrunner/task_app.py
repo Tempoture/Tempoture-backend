ce66e8301f862e67ad63e591af38e54c633bec78
John42506176Linux <johnkongtcheu@gmail.com>
2021-04-07 14:57:54 -0400

    Minor task change

--- taskrunner/task_app.py
+++ taskrunner/task_app.py
@@ -2,8 +2,6 @@ import requests
 
 def store():
     try:
-        hostname = socket.gethostname()
-        ip = socket.gethostbyname(hostname)
         local = f"http://localhost:5000/store_tracks"
         resp = requests.get(local)
         resp.raise_for_status()
