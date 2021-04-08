629c68749f208835abd911cfe1e14eff751fe304
John42506176Linux <johnkongtcheu@gmail.com>
2021-04-08 01:15:05 -0400

    Minor redis changes

--- app.py
+++ app.py
@@ -18,9 +18,8 @@ import os
 app = Flask(__name__)
 CORS(app)
 curr_date = datetime.utcnow().strftime("%m/%d/%Y")
-
-
-app.redis = Redis.from_url(os.environ.get('REDIS_URL') or 'redis://')
+REDIS_URL = os.environ.get('REDISTOGO_URL') or 'redis://'
+app.redis = Redis.from_url(REDIS_URL)
 app.task_queue = rq.Queue('microblog-tasks', connection=app.redis,default_timeout=3600)
 
 
